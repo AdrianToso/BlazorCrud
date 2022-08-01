2ea131800d7633cc1778ec13e863f1908e7faaa2
Adrian <Adrian@ADR-NTBK>
2022-08-01 01:17:06 -0300

    Agregar archivos de proyecto.

---
+++ BlazorCrud/Data/Respuesta.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace BlazorCrud.Data
+{
+    public class Respuesta<T>
+    {
+        public int Success { get; set; }
+        public string ErrCode { get; set; }
+
+        public T Data { get; set; }
+      
+    }
+   
+}
